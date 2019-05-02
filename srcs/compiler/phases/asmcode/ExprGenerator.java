7781c8c0f5663e6a9b9ef1817913077ac4a978c0
sebastian <smeznar@gmail.com>
2019-05-03 00:11:32 +0200

    Started assignment 9 (basic instructions and structure).

--- srcs/compiler/phases/asmcode/ExprGenerator.java
+++ srcs/compiler/phases/asmcode/ExprGenerator.java
@@ -4,6 +4,8 @@
 package compiler.phases.asmcode;
 
 import java.util.*;
+
+import compiler.common.report.Report;
 import compiler.data.layout.*;
 import compiler.data.imcode.*;
 import compiler.data.imcode.visitor.*;
@@ -14,6 +16,43 @@ import compiler.data.asmcode.*;
  */
 public class ExprGenerator implements ImcVisitor<Temp, Vector<AsmInstr>> {
 
-    // TODO
+    @Override
+    public Temp visit(ImcBINOP expr, Vector<AsmInstr> visArg){
+        return null;
+    }
 
+    @Override
+    public Temp visit(ImcUNOP expr, Vector<AsmInstr> visArg){
+        return null;
+    }
+
+    @Override
+    public Temp visit(ImcCALL call, Vector<AsmInstr> visArg){
+        return null;
+    }
+
+    @Override
+    public Temp visit(ImcMEM mem, Vector<AsmInstr> visArg){
+        return null;
+    }
+
+    @Override
+    public Temp visit(ImcNAME name, Vector<AsmInstr> visArg){
+        return null; // cannot happen?
+    }
+
+    @Override
+    public Temp visit(ImcTEMP temp, Vector<AsmInstr> visArg){
+        return temp.temp;
+    }
+
+    @Override
+    public Temp visit(ImcCONST constant, Vector<AsmInstr> visArg){
+        return null; // If it comes to a constant it should be an expression statement.
+    }
+
+    @Override
+    public Temp visit(ImcSEXPR expr, Vector<AsmInstr> visArg){
+        throw new Report.Error("[AsmCode] Cannot have ImcSEXPR in AsmCode phase.");
+    }
 }
--- srcs/compiler/phases/asmcode/StmtGenerator.java
+++ srcs/compiler/phases/asmcode/StmtGenerator.java
@@ -15,6 +15,73 @@ import compiler.common.report.*;
  */
 public class StmtGenerator implements ImcVisitor<Vector<AsmInstr>, Object> {
 
-    // TODO
+    private ExprGenerator exprGenerator;
 
+    private ExprGenerator getExpressionGenerator(){
+        if (exprGenerator == null){
+            exprGenerator = new ExprGenerator();
+        }
+        return exprGenerator;
+    }
+
+    @Override
+    public Vector<AsmInstr> visit(ImcCJUMP cjump, Object visArg){
+        Vector<AsmInstr> instructions = new Vector<>();
+        Vector<Temp> uses = new Vector<>();
+        Vector<Label> jumps = new Vector<>();
+        jumps.add(cjump.posLabel);
+        uses.add(cjump.cond.accept(getExpressionGenerator(), instructions));
+        instructions.add(new AsmOPER("BNZ `l0", uses, null, jumps));
+        return instructions;
+    }
+
+    @Override
+    public Vector<AsmInstr> visit(ImcJUMP jump, Object visArg){
+        Vector<AsmInstr> instructions = new Vector<>();
+        Vector<Label> jumps = new Vector<>();
+        jumps.add(jump.label);
+        instructions.add(new AsmOPER("JMP `l0", null, null, jumps)); //Todo: Fix
+        return instructions;
+    }
+
+    @Override
+    public Vector<AsmInstr> visit(ImcMOVE move, Object visArg){
+        Vector<AsmInstr> instructions = new Vector<>();
+        Vector<Temp> uses = new Vector<>();
+        Vector<Temp> defines = new Vector<>();
+        Vector<Label> jumps = new Vector<>();
+        if (move.dst instanceof ImcTEMP && move.src instanceof ImcTEMP){
+            uses.add(((ImcTEMP) move.src).temp);
+            defines.add(((ImcTEMP) move.dst).temp);
+            instructions.add(new AsmMOVE("STO `d0,`s0,0", uses, defines));
+        } else if (move.src instanceof ImcCONST){
+            Temp dst = move.dst.accept(getExpressionGenerator(), instructions);
+            defines.add(dst);
+            instructions.add(new AsmOPER("", uses, defines, null)); // Todo: add instructions
+        } else if (move.src instanceof ImcNAME && move.dst instanceof ImcTEMP){
+            defines.add(((ImcTEMP) move.dst).temp);
+            // Todo: something with the label
+        }
+        // Todo: others
+        return instructions;
+    }
+
+    @Override
+    public Vector<AsmInstr> visit(ImcESTMT estmt, Object visArg){
+        Vector<AsmInstr> instructions = new Vector<>();
+        estmt.expr.accept(getExpressionGenerator(), instructions);
+        return instructions;
+    }
+
+    @Override
+    public Vector<AsmInstr> visit(ImcLABEL label, Object visArg){
+        Vector<AsmInstr> instructions = new Vector<>();
+        instructions.add(new AsmLABEL(label.label));
+        return instructions;
+    }
+
+    @Override
+    public Vector<AsmInstr> visit(ImcSTMTS stmts, Object visArg){
+        throw new Report.Error( "[AsmCode] Cannot have ImcSTMTS in AsmCode phase.");
+    }
 }
